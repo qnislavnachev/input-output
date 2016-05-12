ca85a7b276dd984f24947b4a0eaeb3425b875d90
Denis Dimitrov <denis.k.dimitrov@gmail.com>
2016-05-12 11:25:40 +0300

    The first task from Input Output

---
+++ task1/consolereader/ConsoleReader.java
@@ -0,0 +1,26 @@
+package task1.consolereader;
+
+import java.util.Scanner;
+
+/**
+ * Created by clouway on 12.05.16.
+ */
+public class ConsoleReader {
+    private Scanner s = new Scanner(System.in);
+
+    public String readString() {
+        return s.nextLine();
+    }
+
+    public int readInt() {
+        return s.nextInt();
+    }
+
+    public char readChar() {
+        return s.next().charAt(0);
+    }
+
+    public double readDouble() {
+        return s.nextDouble();
+    }
+}
---
+++ task1/consolereader/Demo.java
@@ -0,0 +1,18 @@
+package task1.consolereader;
+
+/**
+ * Created by clouway on 12.05.16.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        ConsoleReader reader = new ConsoleReader();
+        System.out.println("Enter a string: ");
+        reader.readString();
+        System.out.println("Enter an int: ");
+        reader.readInt();
+        System.out.println("Enter a char: ");
+        reader.readChar();
+        System.out.println("Enter a double: ");
+        reader.readDouble();
+    }
+}
