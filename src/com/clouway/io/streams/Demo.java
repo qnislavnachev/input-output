773b3e117cf2d0e539ae0b0d8532be3d96699a70
Miroslav Genov <mgenov@gmail.com>
2016-06-09 11:30:21 +0300

    io: add demo for streams usages
    Just a dummy demo and nothing more.

---
+++ src/com/clouway/io/streams/Demo.java
@@ -0,0 +1,10 @@
+package com.clouway.io.streams;
+
+/**
+ * @author Miroslav Genov (miroslav.genov@clouway.com)
+ */
+public class Demo {
+  public static void main(String[] args) {
+    System.out.println("Demo for Streams");
+  }
+}
