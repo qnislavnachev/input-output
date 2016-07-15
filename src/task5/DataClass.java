8d1f96b1159766816aa0410b8ab6ce6e531faaa9
qnislavnachgev <qnislav.nachev@gmail.com>
2016-07-15 17:01:22 +0300

    Fixed task 5 method saveObject and getObject

--- src/task5/DataClass.java
+++ src/task5/DataClass.java
@@ -1,18 +1,32 @@
 package task5;
 
+import org.omg.CORBA.portable.*;
+
 import java.io.*;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class DataClass {
-    public void saveObject(OutputStream out, Object o) throws IOException {
-        ObjectOutputStream outObj = new ObjectOutputStream(out);
+    private ObjectInputStream inObj;
+    private ObjectOutputStream outObj;
+    private OutputStream out;
+    private InputStream in;
+
+    public DataClass(InputStream in, OutputStream out){
+        this.in = in;
+        this.out = out;
+    }
+
+    public void saveObject(Object o) throws IOException {
+        outObj = new ObjectOutputStream(out);
         outObj.writeObject(o);
         System.out.println("Object saved!");
         out.close();
         outObj.close();
     }
 
-    public void getObject(InputStream in) throws IOException, ClassNotFoundException {
-        ObjectInputStream inObj = new ObjectInputStream(in);
+    public void getObject() throws IOException, ClassNotFoundException {
+        inObj = new ObjectInputStream(in);
         SomeObject someObj = (SomeObject) inObj.readObject();
         System.out.println("Object read!");
         in.close();
--- src/task5/Demo.java
+++ src/task5/Demo.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 
 public class Demo {
     public static void main(String[] args) throws IOException,ClassNotFoundException {
-        DataClass data = new DataClass();
         FileOutputStream fileOut = new FileOutputStream("objOut.bin");
         FileInputStream fileIn = new FileInputStream("objOut.bin");
+        DataClass data = new DataClass(fileIn,fileOut);
         SomeObject someObject = new SomeObject(8, 10);
 
-        data.saveObject(fileOut, someObject);
-        data.getObject(fileIn);
+        data.saveObject(someObject);
+        data.getObject();
         System.out.println("Object Size: " + someObject.size + "\n" + "Object Weigth: " + someObject.weigth);
     }
 }
