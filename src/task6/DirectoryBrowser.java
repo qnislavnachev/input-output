03df45b64c44237e84dc8e64ca95433fc6eabe56
qnislavnachgev <qnislav.nachev@gmail.com>
2016-07-18 11:20:32 +0300

    Fixed for and if statment

--- src/task6/DirectoryBrowser.java
+++ src/task6/DirectoryBrowser.java
@@ -8,9 +8,12 @@ public class DirectoryBrowser {
         File[] list = file.listFiles();
         if (file.isFile())
             System.out.println("This is file!");
-        else
-            for (int i = 0; i < list.length; i++)
-                if (file.isDirectory())
+        else {
+            for (int i = 0; i < list.length; i++) {
+                if (file.isDirectory()) {
                     System.out.println(list[i].getName());
+                }
+            }
+        }
     }
 }
